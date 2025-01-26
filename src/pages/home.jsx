3adb722676a1f8a400f9b3ee0affd51c5be65fa5
yan <yanyam.xue@gmail.com>
2025-01-26 17:52:52 -0500

    things are working again

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -131,8 +131,7 @@ export default function App() {
                       <p> Hey! I'm Yan Xue, a passionate software developer and AI enthusiast 
                           studying Computer Engineering at The Univerisity of Waterloo. I'm originally 
                           from Vancouver, BC, but I'll be studying abroad at Waterloo, ON for the next 5 years. I'm 
-                          always excited to improve my skills and explore new experiences. Having said that,
-                          I am welcome to discuss any new oppurtunities! 
+                          always excited to improve my skills and explore new experiences. Feel free to reach out. I am open to any and all oppurtunities! 
                       </p>  
                   </div>
                 </motion.div> 
@@ -284,30 +283,7 @@ export default function App() {
 
       <section id="projects" className="p3"> 
           <div className="project_heading"> Projects </div>
-            <Card.Root flexDirection="row" overflow="hidden" maxW="xl">
-              <Image
-                objectFit="cover"
-                maxW="200px"
-                src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-                alt="Caffe Latte"
-              />
-              <Box>
-                <Card.Body>
-                  <Card.Title mb="2">The perfect latte</Card.Title>
-                  <Card.Description>
-                    Caffè latte is a coffee beverage of Italian origin made with espresso
-                    and steamed milk.
-                  </Card.Description>
-                  <HStack mt="4">
-                    <Badge>Hot</Badge>
-                    <Badge>Caffeine</Badge>
-                  </HStack>
-                </Card.Body>
-                <Card.Footer>
-                  <Button>Buy Latte</Button>
-                </Card.Footer>
-              </Box>
-            </Card.Root>
+            
           <div className="projects"> 
 
           </div> 
