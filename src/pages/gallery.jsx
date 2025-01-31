11ac5127034df7bde24f736212b64a130efd5632
yan <yanyam.xue@gmail.com>
2025-01-31 14:58:26 -0500

    fixed bugs

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -2,18 +2,19 @@
 import React from "react";
 import Typewriter from '../components/typewriter'
 import '../gallery.css'
-import g1 from '../images/g1.png'
-import g2 from '../images/g2.png'
-import g3 from '../images/g3.png'
-import g4 from '../images/g4.png'
-import g5 from '../images/g5.png'
-import g6 from '../images/g6.png'
-import g7 from '../images/g7.png'
-import g8 from '../images/g8.png'
-import g9 from '../images/g9.png'
-import g10 from '../images/g10.png'
-import g11 from '../images/g11.png'
-import g12 from '../images/g12.png'
+
+// import g1 from '../images/g1.png'
+// import g2 from '../images/g2.png'
+// import g3 from '../images/g3.png'
+// import g4 from '../images/g4.png'
+// import g5 from '../images/g5.png'
+// import g6 from '../images/g6.png'
+// import g7 from '../images/g7.png'
+// import g8 from '../images/g8.png'
+// import g9 from '../images/g9.png'
+// import g10 from '../images/g10.png'
+// import g11 from '../images/g11.png'
+// import g12 from '../images/g12.png'
 
 
 
@@ -24,7 +25,7 @@ export default function About() {
         <Typewriter text="Here are some of my favourite photos that highlight my life" speed={50} highlightWord='my life'/>
       </div>
       <div className="gallery"> 
-        <img src={g1} alt="1"/> 
+        {/* <img src={g1} alt="1"/> 
         <img src={g2} alt="2"/> 
         <img src={g3} alt="3"/> 
         <img src={g4} alt="4"/> 
@@ -35,7 +36,7 @@ export default function About() {
         <img src={g9} alt="9"/> 
         <img src={g10} alt="10"/> 
         <img src={g11} alt="11"/> 
-        <img src={g12} alt="12"/> 
+        <img src={g12} alt="12"/>  */}
       </div> 
     </div>
     //create a 2 by 2 grid with fade in and fade out... each photo maybe can click in with a short description 
