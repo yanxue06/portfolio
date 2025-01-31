534d36ce94eaff0852b97ebc96fd9c680bbccdd6
yan <yanyam.xue@gmail.com>
2025-01-31 15:45:28 -0500

    typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './Home.css'; 
+import './home.css'; 
 import Navbar from './components/navbar';
 import './navbar.css';
 
