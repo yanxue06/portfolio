61d59559b85d0d2655c36c97211f20d1e6c13890
yan <yanyam.xue@gmail.com>
2025-02-18 17:21:51 -0500

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from "./components/ui/provider"
+import { Analytics } from '@vercel/analytics/react'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <Provider>
-        <App />
-      </Provider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+    <Analytics />
+  </React.StrictMode>,
 )
