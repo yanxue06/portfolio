7757dceeb058e59172ac77fe5b0e9cf61194e8a8
yan <yanyam.xue@gmail.com>
2025-03-19 01:52:53 -0400

    testing with one photo

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,26 +1,26 @@
 // about.jsx
 import React, { useEffect, useState } from "react";
-import Typewriter from '../components/typewriter'
 
 import '../styles/gallery.css'
 import g1 from '../images/gallery/g1.png'
-import g2 from '../images/gallery/g2.png'
-import g3 from '../images/gallery/g3.png'
-import g4 from '../images/gallery/g4.png'
-import g5 from '../images/gallery/g5.png'
-import g6 from '../images/gallery/g6.png'
-import g7 from '../images/gallery/g7.png'
-import g8 from '../images/gallery/g8.png'
-import g9 from '../images/gallery/g9.png'
-import g10 from '../images/gallery/g10.png'
-import g11 from '../images/gallery/g11.png'
-import g12 from '../images/gallery/g12.png'
-// Import the new images
-import g13 from '../images/gallery/g13.png'
-import g14 from '../images/gallery/g14.png'
-import g15 from '../images/gallery/g15.png'
-import g16 from '../images/gallery/g16.png'
-import g17 from '../images/gallery/g17.png'
+// import g2 from '../images/gallery/g2.png'
+// import g3 from '../images/gallery/g3.png'
+// import g4 from '../images/gallery/g4.png'
+// import g5 from '../images/gallery/g5.png'
+// import g6 from '../images/gallery/g6.png'
+// import g7 from '../images/gallery/g7.png'
+// import g8 from '../images/gallery/g8.png'
+// import g9 from '../images/gallery/g9.png'
+// import g10 from '../images/gallery/g10.png'
+// import g11 from '../images/gallery/g11.png'
+// import g12 from '../images/gallery/g12.png'
+
+// // Import the new images
+// import g13 from '../images/gallery/g13.png'
+// import g14 from '../images/gallery/g14.png'
+// import g15 from '../images/gallery/g15.png'
+// import g16 from '../images/gallery/g16.png'
+// import g17 from '../images/gallery/g17.png'
 import Contacts from '../components/contacts';
 
 import Box from '@mui/material/Box';
@@ -40,23 +40,23 @@ export default function Gallery() {
     const itemData = [
       // Original images
       { img: g1, title: 'Vancouver, BC' },
-      { img: g2, title: 'Vancouver, BC' },
-      { img: g3, title: 'Vancouver, BC' },
-      { img: g4, title: 'Vancouver, BC' },
-      { img: g5, title: 'Waterloo, ON' },
-      { img: g6, title: 'Waterloo, ON' },
-      { img: g7, title: 'Vancouver, BC' },
-      { img: g8, title: 'Toronto, ON' },
-      { img: g9, title: 'Waterloo, ON' },
-      { img: g10, title: 'Vancouver, BC' },
-      { img: g11, title: 'Ontario Skys' },
-      { img: g12, title: 'Vancouver, BC' },
-      // New images
-      { img: g13, title: 'XuanCheng, China' },
-      { img: g14, title: 'Hangzhou, China' },
-      { img: g15, title: 'Anhui, China' },
-      { img: g16, title: 'Anhui, China' },
-      { img: g17, title: 'GOOGLE PLEASE HIRE ME' }
+      // { img: g2, title: 'Vancouver, BC' },
+      // { img: g3, title: 'Vancouver, BC' },
+      // { img: g4, title: 'Vancouver, BC' },
+      // { img: g5, title: 'Waterloo, ON' },
+      // { img: g6, title: 'Waterloo, ON' },
+      // { img: g7, title: 'Vancouver, BC' },
+      // { img: g8, title: 'Toronto, ON' },
+      // { img: g9, title: 'Waterloo, ON' },
+      // { img: g10, title: 'Vancouver, BC' },
+      // { img: g11, title: 'Ontario Skys' },
+      // { img: g12, title: 'Vancouver, BC' },
+      // // New images
+      // { img: g13, title: 'XuanCheng, China' },
+      // { img: g14, title: 'Hangzhou, China' },
+      // { img: g15, title: 'Anhui, China' },
+      // { img: g16, title: 'Anhui, China' },
+      // { img: g17, title: 'GOOGLE PLEASE HIRE ME' }
     ];
     
     // Handle image load
